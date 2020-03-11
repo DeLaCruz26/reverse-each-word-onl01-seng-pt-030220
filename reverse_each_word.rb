f92261f007577d65dae1892c218e1035d8f1a3e6
Dametrik Fick <dametrik.fick@gmail.com>
2020-03-11 00:07:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence1)
   sentence1.split(" ")
     array = []
-    sentence1.each do |sentence1|
+    sentence1.each do |sentence|
        array << sentence1.reverse
      end
      array.join(" ")
